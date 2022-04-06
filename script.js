c300375c012ae5d491f1d9e75be0e1af4d836814
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-06 17:28:59 -0300

    função de pulo do dino

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ const dino = document.querySelector('.dino');
 
 function handleKeyUp (event) {
     if (event.keyCode === 32){
-        console.log('clicou space')
+        jump();
     }
 }   
-    
-document.addEventListener('keyup', handleKeyUp)
+
+//Função de pulo do dino
+function jump(){
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+            //Descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0){
+                    clearInterval(downInterval);
+                } else {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+        //Subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+        };
+    }, 20)
+
+}
+document.addEventListener('keyup', handleKeyUp);
